23ff5f41ae0f52fc19084507a7c09e7d390d4bc1
Rajesh Duggal <rajesh@duggalmail.com>
2024-09-25 20:03:35 -0400

    Add /todos page with htmx support

--- src/main/java/com/rajeshduggal/todos/TodosApplication.java
+++ src/main/java/com/rajeshduggal/todos/TodosApplication.java
@@ -1,15 +1,35 @@
 package com.rajeshduggal.todos;
 
+import java.util.Comparator;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class TodosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosApplication.class, args);
+	}
+
+}
+
+@Controller
+@RequestMapping("/todos")
+class TodoController {
+	private final Set<Todo> todos = new ConcurrentSkipListSet<>(Comparator.comparingInt(Todo::id));
+
+	@GetMapping
+	String todos(Model model) {
+		model.addAttribute("todos", this.todos);
+		return "todos";
 	}
 
 }
